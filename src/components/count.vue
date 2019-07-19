651950816c95488afd4a83e65501517eec0c18e3
scauzml <13828607279@163.com>
2019-07-19 14:28:26 +0800

    one count

---
+++ src/components/count.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+  <div>
+      <button @click="increase">+
+      </button>
+      <input type="text" v-model="a">
+      <button @click="descease">-</button>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'count',
+  props: {
+    msg: String
+  },
+  data(){
+      return {
+          
+          
+              a:0
+        
+
+      }
+  },
+  methods:{
+     increase(){
+      this.a++;
+     },
+     descease(){
+         this.a--;
+     }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
